049b958be8eac962a54200e45d4cef554bf902df
sharathkumarviart <sharathkumarvirat@gmail.com>
2024-02-07 19:07:19 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import "./App.css";
 import Dash from "./components/Dash";
 import TodoHome from "./components/TodoHome";
 
+
+
 function App() {
   return (
+
     <div className="App">
       <div className="main-container">
         {/* <Dash /> */}
+        
         <TodoHome />
       </div>
     </div>
+  
+   
   );
 }
 
